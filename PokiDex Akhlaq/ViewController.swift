52bef4605c92e3df32728892d4dedbef45d2125e
Akhlaq Ahmad <akhlaqahmadofficial@gmail.com>
2018-10-08 00:54:13 +0500

    Initial Commit

---
+++ PokiDex Akhlaq/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokiDex Akhlaq
+//
+//  Created by Akhlaq Ahmad on 10/8/18.
+//  Copyright © 2018 Akhlaq Ahmad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
